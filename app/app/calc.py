f5c9bac3c3a26f860534d67249dbe9d351248918
milimolina.mm <milimolina.mm@gmail.com>
2024-03-08 13:13:34 -0300

    postgres

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,8 +2,12 @@
 Calculator functions
 """
 
-def add(x,y):
-    return x+y
 
-def subtract(x,y):
-    return y-x
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
